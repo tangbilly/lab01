096b1620c04f45ac89149a2ffd69d665e5c70003
Developer <comps381f@ouhk.edu.hk>
2017-09-04 12:16:00 +0800

    96c263c1265002aa4a41cec5c44a337e

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ cors = require('cors');
 
 var express = require('express');
 var app = express();
-var APIKEY = "";  //signup at api.openweathermap.org and obtain an API Key
+var APIKEY = "96c263c1265002aa4a41cec5c44a337e";  //signup at api.openweathermap.org and obtain an API Key
 
 var options = {
     host: 'api.openweathermap.org',
